04623747ab5c8a185113c2056a169517a2b0ab5b
janoski10 <alejandro.acuna09@inacapmail.cl>
2022-05-02 15:55:04 -0400

    Doble Thread

--- HolaMundoHebra/Program.cs
+++ HolaMundoHebra/Program.cs
@@ -13,7 +13,7 @@ namespace HolaMundoHebra
         {
             for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine("Hola Mundo desde hebra {0}", i);
+                Console.WriteLine("Hola Mundo desde hebra {0}", i , Thread.CurrentThread.Name);
                 Thread.Sleep(1000);
             }
         }
@@ -21,7 +21,12 @@ namespace HolaMundoHebra
         {
             Console.WriteLine("Esto esta antes de la hebra");
             Thread t = new Thread(new ThreadStart(ejecutar));
+            t.Name = "Hebra de Arturo";
             t.Start();
+            Thread t2 = new Thread(new ThreadStart(ejecutar));
+            t2.Name = "Hebra de Maria";
+            t2.Start();
+
             Console.WriteLine("Esto se esta ejecutando despues de la hebra");
             Console.ReadKey();
         }
